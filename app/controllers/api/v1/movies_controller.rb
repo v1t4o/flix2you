8974aeb5189068d7831a53a60f71d88e57f1e239
Victor de Oliveira <vitor.oliveira.marinho@gmail.com>
2022-03-25 21:44:09 -0300

    Testa a atualização do catálogo de acordo com o csv disponibilizado na raiz do projeto

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,5 +1,25 @@
+require 'csv'
+
 class Api::V1::MoviesController < Api::V1::ApiController
     def index
         @movies = Movie.all.order(release_year: :desc)
+    end
+
+    def update
+        table_movies = CSV.parse(File.read('netflix_titles.csv'), headers: true)
+        movies = table_movies.map(&:to_h)
+        movies.each do |movie|
+            result = Movie.find_by(index: movie["show_id"])
+            if result.nil?
+                Movie.create!(index: movie["show_id"],
+                              title: movie["title"], genre: movie["type"], director: movie["director"],
+                              cast: movie["cast"], country: movie["country"],
+                              date_added: movie["date_added"], release_year: movie["release_year"],
+                              rating: movie["rating"], duration: movie["duration"], 
+                              listed_in: movie["listed_in"], description: movie["description"]
+                             )
+            end
+        end
+        render json: {"msg": "O catálogo está atualizado."}, status: 200
     end
 end
--- spec/requests/movies_api_spec.rb
+++ spec/requests/movies_api_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'MovieModel API' do
     context 'GET /api/v1/movies' do
-        it 'sucessfully' do
+        it 'successfully' do
             #Arrange
             Movie.create!(index: "840c7cfc-cd1f-4094-9651-688457d97fa4",
                           title: "13 Reasons Why", genre: "TV Show", release_year: "2020",
@@ -19,6 +19,17 @@ describe 'MovieModel API' do
             expect(response.status).to eq 200
             expect(response.content_type).to include('application/json')
             expect(parsed_response[0]["title"]).to eq '13 Reasons Why'
+        end
+    end
+
+    context 'POST /api/v1/movies' do
+        it 'successfully' do
+            get '/api/v1/movies/update'
+
+            expect(response.status).to eq 200
+            parsed_response = JSON.parse(response.body)
+            expect(response.content_type).to include('application/json')
+            expect(response.body).to include('O catálogo está atualizado.')
         end
     end
 end
